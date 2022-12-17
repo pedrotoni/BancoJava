9f952776f32f6cca5cce48f1286c301209158a79
Pedro de Toni <pedrovdetoni@gmail.com>
2022-12-17 18:44:22 -0300

    adição de metodos para jogada do player2 e validação de jogadas

--- JavaDaVelha.java
+++ JavaDaVelha.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 
 public class JavaDaVelha {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("JOGO DA VELHA");
-        System.out.println("Escolha números de 1 a 9 para fazer sua jogada!\nUtilize o guia abaixo: \n");
-        System.out.println("[ 1 ] [ 2 ] [ 3 ]\n[ 4 ] [ 5 ] [ 6 ]\n[ 7 ] [ 8 ] [ 9 ]\n");
+        System.out.println("Digite uma coordenada para fazer sua jogada!\nUtilize o guia abaixo: \n");
+        System.out.println("[ 00 ] [ 01 ] [ 02 ]\n[ 10 ] [ 11 ] [ 12 ]\n[ 20 ] [ 21 ] [ 22 ]\n");
         Scanner sc = new Scanner(System.in);
         Character[][] tabuleiro = new Character[3][3];
 
         jogadaPlayerUm(tabuleiro,sc);
         mostraTabuleiro(tabuleiro);
-        jogadaPlayerUm(tabuleiro,sc);
+        jogadaPlayerDois(tabuleiro,sc);
         mostraTabuleiro(tabuleiro);
 
 
 
     }
-
     public static void mostraTabuleiro(Character[][] tabuleiro) {
         for (Character[] linha : tabuleiro) {
             System.out.print("[ ");
@@ -39,21 +38,60 @@ public class JavaDaVelha {
             System.out.println();
         }
     }
-
-    public static void jogadaPlayerUm (Character[][] tabuleiro, Scanner sc) {
-        System.out.println("Faça sua jogada!");
-        String escolha = sc.nextLine();
-        switch (escolha) {
-            case "1" -> tabuleiro[0][0] = 'X';
-            case "2" -> tabuleiro[0][1] = 'X';
-            case "3" -> tabuleiro[0][2] = 'X';
-            case "4" -> tabuleiro[1][0] = 'X';
-            case "5" -> tabuleiro[1][1] = 'X';
-            case "6" -> tabuleiro[1][2] = 'X';
-            case "7" -> tabuleiro[2][0] = 'X';
-            case "8" -> tabuleiro[2][1] = 'X';
-            case "9" -> tabuleiro[2][2] = 'X';
+    public static void jogadaPlayerUm(Character[][] tabuleiro, Scanner sc) {
+        System.out.println("Faça sua jogada! (Player1)");
+        String escolha1;
+        while (true) {
+            escolha1 = sc.nextLine();
+            if (verificaJogada(tabuleiro, escolha1)) {
+                break;
+            }
+            System.out.println("Ops! Jogada inválida");
+        }
+        jogada(tabuleiro,escolha1,'X');
+        System.out.println("Player 1 escolheu coordenada "+escolha1);
+    }
+    public static void jogadaPlayerDois(Character[][] tabuleiro, Scanner sc) {
+        System.out.println("Faça sua jogada! (Player2)");
+        String escolha2;
+        while (true) {
+            escolha2 = sc.nextLine();
+            if (verificaJogada(tabuleiro, escolha2)) {
+                break;
+            }
+            System.out.println("Ops! Jogada Inválida!");
+        }
+        jogada(tabuleiro,escolha2,'O');
+        System.out.println("Player 2 escolheu coordenada "+escolha2);
+    }
+    public static void jogada(Character[][] tabuleiro, String coordenada, char XouO) {
+        switch (coordenada) {
+            case "00" -> tabuleiro[0][0] = XouO;
+            case "01" -> tabuleiro[0][1] = XouO;
+            case "02" -> tabuleiro[0][2] = XouO;
+            case "10" -> tabuleiro[1][0] = XouO;
+            case "11" -> tabuleiro[1][1] = XouO;
+            case "12" -> tabuleiro[1][2] = XouO;
+            case "20" -> tabuleiro[2][0] = XouO;
+            case "21" -> tabuleiro[2][1] = XouO;
+            case "22" -> tabuleiro[2][2] = XouO;
             default -> System.out.println("Jogada Inválida!");
         }
     }
+
+    public static boolean verificaJogada(Character[][] tabuleiro, String coordenada) {
+        return switch (coordenada) {
+            case "00" -> tabuleiro[0][0] == null;
+            case "01" -> tabuleiro[0][1] == null;
+            case "02" -> tabuleiro[0][2] == null;
+            case "10" -> tabuleiro[1][0] == null;
+            case "11" -> tabuleiro[1][1] == null;
+            case "12" -> tabuleiro[1][2] == null;
+            case "20" -> tabuleiro[2][0] == null;
+            case "21" -> tabuleiro[2][1] == null;
+            case "22" -> tabuleiro[2][2] == null;
+            default -> false;
+        };
+    }
+
 }
